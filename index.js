bb8605b5aa3263719543069182a9a3a17ec00ee3
Winston Pemberton <winstonpemberton@gmail.com>
2020-12-03 23:08:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function destructivelyUpdateDriverWithKeyAndValue(obj, key, value){
 
 function deleteFromDriverByKey(obj, key){
   let newObj =  Object.assign({},object)
-  delete obj.key;
+  delete newObj.key;
   return newObj
 }
 
